8a33d5fd01afac6f3e343418420c5bc257ad3b99
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-02-21 20:15:55 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,56 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.addEventListener("DOMContentLoaded", function() {
+  const widthInput = document.getElementById("widthInput");
+  const heightInput = document.getElementById("heightInput");
+  const carta = document.getElementById("carta");
+
+  function generarCarta() {
+    const palos = ["♠", "♣", "♥", "♦"];
+    const valores = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
+
+    const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+    const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+
+    const width = widthInput.value + "px";
+    const height = heightInput.value + "px";
+
+    carta.style.width = width;
+    carta.style.height = height;
+    carta.classList = "card " + paloAleatorio.toLowerCase();
+    carta.querySelector(".top-left").innerHTML = paloAleatorio;
+    carta.querySelector(".bottom-right").innerHTML = paloAleatorio;
+    carta.querySelector(".number").innerHTML = valorAleatorio;
+
+    if (paloAleatorio === "♥" || paloAleatorio === "♦") {
+      carta.querySelector(".top-left").style.color = "red";
+      carta.querySelector(".bottom-right").style.color = "red";
+    } else {
+      carta.querySelector(".top-left").style.color = "black";
+      carta.querySelector(".bottom-right").style.color = "black";
+    }
+  }
+
+  generarCarta();
+
+  document
+    .getElementById("nuevaCartaBtn")
+    .addEventListener("click", function() {
+      generarCarta();
+    });
+});
